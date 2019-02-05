c3d7368003b602340fa404e86eae96659b4867ad
arm923i <44763562+arm923i@users.noreply.github.com>
2019-02-05 05:09:29 +0200

    Update gamepad-controller.js

--- gamepad-controller.js
+++ gamepad-controller.js
@@ -1,43 +1,125 @@
+/* Extension demonstrating a blocking reporter block */
+
 (function(ext) {
-    // Cleanup  function when the extension is unloaded
-    ext._shutdown = function() {
-      controller = null;
-      connected = false;
-    };  
+  var APPID = '960f7f58abbc5c98030d1899739c1ba8';
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-     ext._getStatus = function() {
-      if (!connected)
-        return { status:1, msg:'Disconnected' };
-      else
-        return { status:2, msg:'Connected' };
-    };
+  var cacheDuration = 1800000 //ms, 30 minutes
+  var cachedTemps = {};
 
-    ext.get_temp = function(location) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'https://openweathermap.org/data/2.5/weather?q='+location+'&appid=b6907d289e10d714a6e88b30761fae22',
-              dataType: 'jsonp',
-              success: function( weather_data ) {
-                  // Got the data - parse it and return the temperature
-                  temperature = weather_data['main']['temp'];
-                  return temperature;
-              }
-        });
-    }; 
+  var units = 'imperial';
 
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            ['R', 'current temperature in city %m.city', 'get_temp', 'Odessa'],
-        ],
-        menus: {
-            city: ['Odessa', 'Kherson']
-        },  
-         url: 'http://arm923i.github.io/gamepad-scratch-extension/'
-    };
+  function getWeatherData(weatherData, type) {
+    var val = null;
+    switch (type) {
+      case 'temperature':
+        val = weatherData.main.temp;
+        if (units === 'metric')
+          val = (val - 32) * (5/9)
+        val = Math.round(val);
+        break;
+      case 'weather':
+        val = weatherData.weather[0].description;
+        break;
+      case 'humidity':
+        val = weatherData.main.humidity;
+        break;
+      case 'wind speed':
+        val = weatherData.wind.speed;
+        if (units === 'imperial')
+          val *= 2.23694;
+        if (Math.round(val) !== val)
+          val = val.toFixed(1);
+        break;
+      case 'cloudiness':
+        val = weatherData.clouds.all;
+        break;
+    }
+    return(val);
+  }
 
-    // Register the extension
-    ScratchExtensions.register('Weather extension', descriptor, ext);
-}); 
+  function fetchWeatherData(location, callback) {
+
+    if (location in cachedTemps &&
+        Date.now() - cachedTemps[location].time < cacheDuration) {
+      //Weather data is cached
+      callback(cachedTemps[location].data);
+      return;
+    }
+
+    // Make an AJAX call to the Open Weather Maps API
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather',
+      data: {q: location, units: 'imperial', appid: APPID},
+      dataType: 'jsonp',
+      success: function(weatherData) {
+        //Received the weather data. Cache and return the data.
+        cachedTemps[location] = {data: weatherData, time: Date.now()};
+        callback(weatherData);
+      }
+    });
+  }
+
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = function() {};
+
+  // Status reporting code
+  // Use this to report missing hardware, plugin or unsupported browser
+  ext._getStatus = function() {
+    return {status: 2, msg: 'Ready'};
+  };
+
+  ext.getWeather = function(type, location, callback) {
+    fetchWeatherData(location, function(data) {
+      var val = getWeatherData(data, type);
+      callback(val);
+    });
+  };
+
+  ext.whenWeather = function(type, location, op, val) {
+    if (!cachedTemps[location]) {
+      //Weather data not cached
+      //Fetch it and return false for now
+      fetchWeatherData(location, function(){});
+      return false;
+    }
+    //Weather data is cached, no risk of blocking
+    var data = getWeatherData(cachedTemps[location].data, type);
+    switch (op) {
+      case '<':
+        return (data < val);
+      case '=':
+        return (data == val);
+      case '>':
+        return (data > val);
+    }
+  };
+
+  ext.setUnits = function(format) {
+    units = format;
+    return;
+  };
+
+  ext.getUnits = function() {
+    return units;
+  };
+
+  // Block and block menu descriptions
+  var descriptor = {
+    blocks: [
+      ['R', '%m.reporterData in %s', 'getWeather', 'temperature', 'Boston, MA'],
+      ['h', 'when %m.eventData in %s is %m.ops %n', 'whenWeather', 'temperature', 'Boston, MA', '>', 80],
+      [' ', 'set units to %m.units', 'setUnits', 'imperial'],
+      ['r', 'unit format', 'getUnits']
+    ],
+    menus: {
+      reporterData: ['temperature', 'weather', 'humidity', 'wind speed', 'cloudiness'],
+      eventData: ['temperature', 'humidity', 'wind speed', 'cloudiness'],
+      ops: ['>','=', '<'],
+      units: ['imperial', 'metric']
+    }
+  };
+
+  // Register the extension
+  ScratchExtensions.register('Weather extension', descriptor, ext);
+
+})({});
