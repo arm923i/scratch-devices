46b4e63d5dfbcac68a924ce536f56c120863406f
arm923i <44763562+arm923i@users.noreply.github.com>
2019-06-12 22:28:39 +0300

    Update arm_controller_twin.js

--- res/arm_controller_twin.js
+++ res/arm_controller_twin.js
@@ -6,17 +6,25 @@
 
 (function(ext) {
 
+  var DEADZONE = 8000 / 32767;
+  
   var buttons = [
+    ["left top", 4],
+    ["left bottom", 6],
+    ["right top", 5],
+    ["right bottom", 7],
+    ["left stick", 10],
+    ["right stick", 11],
     ["A", 0],
     ["B", 1],
     ["X", 2],
     ["Y", 3],
-    ["left petal", 4],
-    ["right petal", 5],
-    ["left bottom", 6],
-    ["right bottom", 7],
     ["select", 8],
     ["start", 9],
+    ["up", 12],
+    ["down", 13],
+    ["left", 14],
+    ["right", 15],
   ];
 
   var buttonMenu = [];
@@ -36,7 +44,7 @@
   ext.gamepad_2 = null;
 
   ext.tick = function() {
-    ext.gamepad = (navigator.getGamepads && navigator.getGamepads()[0]);
+    ext.gamepad   = (navigator.getGamepads && navigator.getGamepads()[0]);
     ext.gamepad_2 = (navigator.getGamepads && navigator.getGamepads()[1]);
     window.requestAnimationFrame(ext.tick);
   };
@@ -91,18 +99,40 @@
       case "pedals": return x.toFixed(2)*5;
     }
   };
+  ext.getStick_2 = function(what, stick) {
+    var x, y;
+    switch (stick) {
+      case "left":  x = ext.gamepad_2.axes[0]; y = -ext.gamepad_2.axes[1]; break;
+      case "right": x = ext.gamepad_2.axes[2]; y = -ext.gamepad_2.axes[3]; break;
+    }
+    if (-DEADZONE < x && x < DEADZONE) x = 0;
+    if (-DEADZONE < y && y < DEADZONE) y = 0;
 
+    switch (what) {
+      case "direction":
+        if (x === 0 && y === 0) {
+          // report the stick's previous direction
+          return ext.stickDirection[stick];
+        }
+        var value = 180 * Math.atan2(x, y) / Math.PI;
+        ext.stickDirection[stick] = value;
+        return value;
+      case "force":
+        return Math.sqrt(x*x + y*y) * 100;
+    }
+  };
   var descriptor = {
     blocks: [
       ["b", "modules installed?", "installed"],
       ["b", "gamepad #1 button %m.button pressed", "getButton", "X"],
       ["b", "gamepad #2 button %m.button pressed", "getButton_2", "X"],
-      ["r", "gamepad #1 %m.stick direction", "getStick", "wheel"],
-      ["r", "gamepad #2 %m.stick direction", "getStick", "wheel"],
+      ["r", "gamepad #1 %m.axisValue of %m.stick stick", "getStick", "direction", "left"],
+      ["r", "gamepad #2 %m.axisValue of %m.stick stick", "getStick_2", "direction", "left"],
     ],
     menus: {
       button: buttonMenu,
-      stick: ["wheel","pedals"],
+      stick: ["left", "right"],
+      axisValue: ["direction", "force"],
     },
   };
 
