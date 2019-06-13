70f8a3522302b52ec78c57babf6303102e147b8f
arm923i <44763562+arm923i@users.noreply.github.com>
2019-06-13 04:15:39 +0300

    Update scroll-to-top.js

--- assets/js/scroll-to-top.js
+++ assets/js/scroll-to-top.js
@@ -1,4 +1,13 @@
 $('.scrollup').click(function(){
 	$("html, body").animate({ scrollTop: 0}, 600);
-	return false;
-}); 
+		return false;
+});
+
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
