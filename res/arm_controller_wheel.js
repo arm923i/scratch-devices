63194518f3233c83330f90fc74439c3c6e3dfc1b
arm923i <44763562+arm923i@users.noreply.github.com>
2019-06-22 19:40:04 +0300

    Update arm_controller_wheel.js

--- res/arm_controller_wheel.js
+++ res/arm_controller_wheel.js
@@ -89,6 +89,7 @@
       button: buttonMenu,
       stick: ["wheel","pedals"],
     },
+    url: 'https://arm923i.github.io/scratch-devices/'
   };
 
   ScratchExtensions.register("Wheel GamePad", descriptor, ext);
