97610dd4664ce946d7a86a02b8edcf4e4d7ba1c0
arm923i <44763562+arm923i@users.noreply.github.com>
2019-06-25 21:23:07 +0300

    Update arm_controller_new.js

--- res/arm_controller_new.js
+++ res/arm_controller_new.js
@@ -17,14 +17,14 @@
        ['B', 1],
        ['X', 2],
        ['Y', 3],
-       ['left top', 4],
-       ['left bottom', 6],
-       ['right top', 5],
-       ['right bottom', 7],
-       ['select', 8],
-       ['start', 9],
-       ['left stick', 10],
-       ['right stick', 11]
+       ['Left Top', 4],
+       ['Left Bottom', 6],
+       ['Right Top', 5],
+       ['Right Bottom', 7],
+       ['Select', 8],
+       ['Start', 9],
+       ['Left Stick', 10],
+       ['Right Stick', 11]
     ];
    
 	 var buttonList = [];
@@ -67,7 +67,11 @@
 	    if (controllers.length == 0) return {
 	      status: 1,
 	      msg: "Plug in a device and press any button",
-	    };    	
+	    };
+	    if (controllers == {}) return {
+	      status: 1,
+	      msg: "Plug in a device and press any button1",
+	    };        	
         return {
             status: 2,
             msg: 'Ready'
@@ -79,148 +83,87 @@
 		return controllers[indxs].id;
 	};
 
-    ext.stickDirection = { left: 90, right: 90 };
+	// Deadzone 
+    var dz = 8000 / 32767; 
 
-    var dz = 8000 / 32767; // Deadzone
-
-     ext.getButtonPressing = function(indx, name) {
-     	var indxs = parseInt(indx)-1;
-	    var index = buttonNames[name];
-	    var button = controllers[indxs].buttons[index];
-	    return button.pressed;
-	  };
-
-     ext.getStickState = function(indx, stick, what) {
-     	var indxs = parseInt(indx)-1;
-	    var x, y;
-	    switch (stick) {
-	      case 'Left':  x = controllers[indxs].axes[0]; y = -controllers[indxs].axes[1]; break;
-	      case 'Right': x = controllers[indxs].axes[5]; y = -controllers[indxs].axes[2]; break;
-	    }
-	    if (-dz < x && x < dz) x = 0;
-        if (-dz < y && y < dz) y = 0;
-	    switch (what) {
-	      case 'Direction': 
-	      	var value = (180 * Math.atan2(x, y) / Math.PI); 
-	      	ext.stickDirection[stick] = value;
-	        return value.toFixed(2);
-	      break;
-	      case 'ForceX': return x.toFixed(2)*5; break;
-	      case 'ForceY': return y.toFixed(2)*5; break;
-	    }
-	  };
+	ext.getButtonPressing = function(indx, name) {
+		var indxs = parseInt(indx)-1, 
+			index = buttonNames[name],
+			button = controllers[indxs].buttons[index];
+		return button.pressed;
+	};
     
-    ext.getStickState_2 = function(indx, stick, af) {
-    	var indxs = parseInt(indx)-1;
-        var x, y;
+    ext.getStickState = function(indx, stick, what) {
+    	var indxs = parseInt(indx)-1, x, y;
         switch (stick) {
             case 'Left': x = controllers[indxs].axes[0]; y = -controllers[indxs].axes[1]; break;
             case 'Right': x = controllers[indxs].axes[2]; y = -controllers[indxs].axes[5]; break;
         }
         if (-dz < x && x < dz) x = 0;
         if (-dz < y && y < dz) y = 0;
-        switch(af) {
+        switch(what) {
             case 'Angle': return(value = 180 * Math.atan2(x, y) / Math.PI); break;
             case 'Force': return Math.sqrt(x*x + y*y).toFixed(2); break;
+            case 'ForceX': return x.toFixed(2); break;
+	      	case 'ForceY': return y.toFixed(2); break;
         }
     };
 
-    ext.getStickPosition = function(indx, s, hv) {
+    ext.getStickPosition = function(indx, stick, hv) {
     	var indxs = parseInt(indx)-1;
-        return (controllers[indxs].axes[(['LeftX', 'LeftY', 'RightX', '', '', 'RightY'].indexOf(s + hv))].toFixed(2)); 
+        return (controllers[indxs].axes[(['LeftHorizontal', 'LeftVertical', 'RightHorizontal', '', '', 'RightVertical'].indexOf(stick + hv))].toFixed(2)); 
     };
 
-    ext.getStickDirection = function(indx, s, hvb) { 
-    	var indxs = parseInt(indx)-1;
-        let output = '';
-        if (s == 'Left') {
-            if (hvb == 'XY' || hvb == 'Y') {
-                if (controllers[indxs].axes[1] < -.5) {
-                    output += 'Up '
-                } else if (controllers[indxs].axes[1] > .5) {
-                    output += 'Down '
-                }
+    ext.getStickDirection = function(indx, stick, hvb) { 
+    	var indxs = parseInt(indx)-1; let output = '';
+        if (stick == 'Left') {
+            if (hvb == 'Both' || hvb == 'Vertical') {
+                if (controllers[indxs].axes[1] < -.5) { output += 'Up ' } 
+                else if (controllers[indxs].axes[1] > .5) { output += 'Down ' }
             }
-            if (hvb == 'XY' || hvb == 'X') {
-                if (controllers[indxs].axes[0] < -.5) {
-                    output += 'Left'
-                } else if (controllers[indxs].axes[0] > .5) {
-                    output += 'Right'
-                }
+            if (hvb == 'Both' || hvb == 'Horizontal') {
+                if (controllers[indxs].axes[0] < -.5) { output += 'Left' } 
+                else if (controllers[indxs].axes[0] > .5) { output += 'Right' }
             }
         };
-        if (s == 'Right') {
-            if (hvb == 'XY' || hvb == 'Y') {
-                if (controllers[indxs].axes[5] < -.5) {
-                    output += 'Up '
-                } else if (controllers[indxs].axes[5] > .5) {
-                    output += 'Down '
-                }
+        if (stick == 'Right') {
+            if (hvb == 'Both' || hvb == 'Vertical') {
+                if (controllers[indxs].axes[5] < -.5) { output += 'Up ' } 
+                else if (controllers[indxs].axes[5] > .5) { output += 'Down ' }
             }
-            if (hvb == 'XY' || hvb == 'X') {
-                if (controllers[indxs0].axes[2] < -.5) {
-                    output += 'Left'
-                } else if (controllers[indxs].axes[2] > .5) {
-                    output += 'Right'
-                }
+            if (hvb == 'Both' || hvb == 'Horizontal') {
+                if (controllers[indxs0].axes[2] < -.5) { output += 'Left' } 
+                else if (controllers[indxs].axes[2] > .5) { output += 'Right' }
             }
         };
         return (output);
     };
 
-    ext.getStickFacing = function(indx, s, dir) {
+    ext.getStickFacing = function(indx, stick, dir) {
     	var indxs = parseInt(indx)-1;
-        if (s == 'Left') {
-            if (dir == 'Up') {
-                return (controllers[indxs].axes[1] < -.5)
-            }
-            if (dir == 'Down') {
-                return (controllers[indxs].axes[1] > .5)
-            }
-            if (dir == 'Left') {
-                return (controllers[indxs].axes[0] < -.5)
-            }
-            if (dir == 'Right') {
-                return (controllers[indxs].axes[0] > .5)
-            }
-            if (dir == 'UpLeft') {
-                return (controllers[indxs].axes[1] < -.5 && (controllers[indxs].axes[0] < -.5))
-            }
-            if (dir == 'UpRight') {
-                return (controllers[indxs].axes[1] < -.5 && (controllers[indxs].axes[0] > .5))
-            }
-            if (dir == 'DownLeft') {
-                return (controllers[indxs].axes[1] > .5 && (controllers[indxs].axes[0] < -.5))
-            }
-            if (dir == 'DownRight') {
-                return (controllers[indxs].axes[1] > .5 && (controllers[indxs].axes[0] > .5))
-            }
+        if (stick == 'Left') {
+    	    switch(dir) {
+	            case 'Up': 			return (controllers[indxs].axes[1] < -.5); break;
+	            case 'Down': 		return (controllers[indxs].axes[1] > .5);  break;
+	            case 'Left': 		return (controllers[indxs].axes[0] < -.5); break;
+		      	case 'Right': 		return (controllers[indxs].axes[0] > .5);  break;
+		      	case 'Up Left': 	return (controllers[indxs].axes[1] < -.5 && (controllers[indxs].axes[0] < -.5)); break;
+		      	case 'Up Right': 	return (controllers[indxs].axes[1] < -.5 && (controllers[indxs].axes[0] > .5));  break;
+		      	case 'Down Left': 	return (controllers[indxs].axes[1] > .5  && (controllers[indxs].axes[0] < -.5)); break;
+		      	case 'Down Right': 	return (controllers[indxs].axes[1] > .5  && (controllers[indxs].axes[0] > .5));  break;
+	        }
         }
-        if (s == 'Right') {
-            if (dir == 'Up') {
-                return (controllers[indxs].axes[5] < -.5)
-            }indxs
-            if (dir == 'Down') {
-                return (controllers[indxs].axes[5] > .5)
-            }
-            if (dir == 'Left') {
-                return (controllers[indxs].axes[2] < -.5)
-            }
-            if (dir == 'Right') {
-                return (controllers[indxs].axes[2] > .5)
-            }
-            if (dir == 'UpLeft') {
-                return (controllers[indxs].axes[5] < -.5 && (controllers[indxs].axes[2] < -.5))
-            }
-            if (dir == 'UpRight') {
-                return (controllers[indxs].axes[5] < -.5 && (controllers[indxs].axes[2] > .5))
-            }
-            if (dir == 'DownLeft') {
-                return (controllers[indxs].axes[5] > .5 && (controllers[indxs].axes[2] < -.5))
-            }
-            if (dir == 'DownRight') {
-                return (controllers[indxs].axes[5] > .5 && (controllers[indxs].axes[2] > .5))
-            }
+        if (stick == 'Right') {
+        	switch(dir) {
+	            case 'Up': 			return (controllers[indxs].axes[5] < -.5); break;
+	            case 'Down': 		return (controllers[indxs].axes[5] > .5);  break;
+	            case 'Left': 		return (controllers[indxs].axes[2] < -.5); break;
+		      	case 'Right': 		return (controllers[indxs].axes[2] > .5);  break;
+		      	case 'Up Left': 	return (controllers[indxs].axes[5] < -.5 && (controllers[indxs].axes[2] < -.5)); break;
+		      	case 'Up Right': 	return (controllers[indxs].axes[5] < -.5 && (controllers[indxs].axes[2] > .5));  break;
+		      	case 'Down Left': 	return (controllers[indxs].axes[5] > .5  && (controllers[indxs].axes[2] < -.5)); break;
+		      	case 'Down Right': 	return (controllers[indxs].axes[5] > .5  && (controllers[indxs].axes[2] > .5));  break;
+	        }
         }
     };
 
@@ -231,24 +174,22 @@
 			['h', 'When GP# %m.devices button %m.buttons pressed', 	'getButtonPressing', '1', 'X'],
 			['b', 'GP# %m.devices button %m.buttons pressed',        'getButtonPressing', '1',	'X'],
 			['-'],
-			['r', 'GP# %m.devices %m.sticks stick %m.hv position', 	 'getStickPosition',  '1', 'Left', 'X'],
-			['r', 'GP# %m.devices %m.sticks stick %m.hvb direction', 'getStickDirection', '1', 	'Left', 'Both'],
+			['h', 'When GP# %m.devices %m.sticks stick is facing %m.directions', 	'getStickFacing', '1', 	'Left', 'Up'],
+			['b', 'GP# %m.devices %m.sticks stick is facing %m.directions?',	    'getStickFacing', '1',	'Left', 'Up'], 
 			['-'],
-			['h', 'When GP# %m.devices %m.sticks stick is facing %m.dir', 	'getStickFacing', '1', 	'Left', 'Up'],
-			['b', 'GP# %m.devices %m.sticks stick is facing %m.dir?',	    'getStickFacing', '1',	'Left', 'Up'], 
-			['-'],  
-			['r', 'GP# %m.devices %m.sticks stick %m.aefe',	     'getStickState_2', '1',	'Left', 'Angle'],   
-			['r', 'GP# %m.devices %m.sticks stick %m.axisValue', 'getStickState', '1',	'Left', 'Direction']
+			['r', 'GP# %m.devices %m.sticks stick %m.hv position', 	 'getStickPosition',  '1', 'Left', 'Horizontal'],
+			['r', 'GP# %m.devices %m.sticks stick %m.hvb direction', 'getStickDirection', '1', 	'Left', 'Both'],
+			['-'], 
+			['r', 'GP# %m.devices %m.sticks stick %m.axisValue', 'getStickState', '1', 'Left', 'Angle']
 		],
 		menus: {
 			devices: 	controllerList,
 			buttons: 	buttonList,
 			sticks: 	['Left', 'Right'],
-			axisValue: 	['Direction', 'ForceX', 'ForceY'],
-			hv: 		['X', 'Y'],
-			hvb: 		['X', 'Y', 'XY'],
-			dir: 		['Up', 'Down', 'Left', 'Right', 'UpLeft', 'UpRight', 'DownLeft', 'DownRight'],
-			aefe: 		['Angle', 'Force']
+			axisValue: 	['Angle', 'Force', 'ForceX', 'ForceY'],
+			hv: 		['Horizontal', 'Vertical'],
+			hvb: 		['Horizontal', 'Vertical', 'Both'],
+			directions: ['Up', 'Down', 'Left', 'Right', 'Up Left', 'Up Right', 'Down Left', 'Down Right']
 		},
 		url: 'https://arm923i.github.io/scratch-devices/',
 		displayName: '@arm923i Gamepad'
