3413a89fb9a855138e2e9a5e91dc34a3b84f0c94
arm923i <44763562+arm923i@users.noreply.github.com>
2019-07-04 05:46:28 +0300

    Update arm_arduino_firmware_humidity_sensor.ino

--- res/arm_arduino_firmware_humidity_sensor.ino
+++ res/arm_arduino_firmware_humidity_sensor.ino
@@ -9,12 +9,26 @@ const int humidityPin = A0;
 const int thresholdValue = 500;
  
 void setup(){
+
   pinMode(rainPin, INPUT);
+
+  memset(analogReadVals, 0, sizeof(analogReadVals));
+  memset(pinModes, 0, sizeof(pinModes));
+
   Serial.begin(9600);
 }
  
 void loop() {
+
   int sensorValue = analogRead(humidityPin);
-  Serial.print(sensorValue);
+
+  for (i=0; i<=5; i++) {
+    analogReadVals[i] = map(analogRead(i), 0, 1023, 0, 255);
+  }
+
+  Serial.write(CMD_ANALOG_READ);
+  Serial.write(analogReadVals, 6);
+  
   delay(1000);
+
 }
